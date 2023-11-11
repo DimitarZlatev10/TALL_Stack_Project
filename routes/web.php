56de7775bab2f2697f2f4c710637dcd9c6bff707
dimitarzlatev <dimitarzlatev02@gmail.com>
2023-11-11 19:52:57 +0200

    creating home and about views and adding routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Livewire\About;
+use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Home::class);
+Route::get('/about', About::class);
